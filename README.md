9efc3255df7ed8e3aaec9ea752cd9395068782d6
Spamakin <anakin@icalyx.com>
2021-12-07 15:02:34 -0600

    believe it or not more formatting + updated README with goals

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# Power-Resilience" 
+# Power-Resilience
+
+## Coding Goals:
+* Test cases (longer term transition to pytest / some other automated testing)
+* Dynamic Programming brute force
+* Add comments just so that we are all on the same page
+* Implement some other more complicated heuristics
--- main.py
+++ main.py
@@ -2,41 +2,6 @@ from graph import graph
 import algos
 import numpy as np
 
-# graphDict = {
-#    "numNodes": 4,
-#    "nodeWeight": [10, 20, 30, 40],
-#    "edges": [(0, 1, 1.0), (2, 3, 1.1)],
-# }
-#
-# g = graph.FromDict(graphDict)
-#
-# print("================================")
-# print("The adjacency list")
-# g.printAdjacencyList()
-# print("================================")
-# print("The edge matrix")
-# g.printEdgeStrength()
-# print("================================")
-# print("Node weight")
-# g.printNodeWeight()
-# print()
-#
-# completeGraph = {
-#    "numNodes": 2,
-#    "nodeWeight": [0, 1],
-#    "edges": [(0, 1, 1.0), (1, 0, 2.0)]
-# }
-# complete = graph.FromDict(completeGraph)
-# print(graph.isComplete(complete))
-#
-# incompleteGraph = {
-#    "numNodes": 2,
-#    "nodeWeight": [0, 1],
-#    "edges": [(1, 0, 2.0)]
-# }
-# incomplete = graph.FromDict(incompleteGraph)
-# print(graph.isComplete(incomplete))
-
 
 def benchmark(n: int, rounds: int) -> None:
     inOrder: tuple[int, ...] = tuple(i for i in range(n))
@@ -67,7 +32,13 @@ def benchmark(n: int, rounds: int) -> None:
     for br, io, ro, nn, gr in zip(
         brute_forces, in_orders, random_orders, nearest_ns, greedy_orders
     ):
-        print(f"{br : <22}{io : <22}" + f"{ro : <22}" + f"{nn : <22}" + f"{gr : <22}")
+        print(
+            f"{br : <22}"
+            + f"{io : <22}"
+            + f"{ro : <22}"
+            + f"{nn : <22}"
+            + f"{gr : <22}"
+        )
 
     print()
     print(
@@ -118,7 +89,8 @@ def benchmark(n: int, rounds: int) -> None:
     nn_av = nn_sum / rounds
     greedy_av = greedy_sum / rounds
     print(
-        f"{'' : <22}{str(io_av) + '%' : <22}"
+        f"{'' : <22}"
+        + f"{str(io_av) + '%' : <22}"
         + f"{str(rand_av) + '%' : <22}"
         + f"{str(nn_av) + '%' : <22}"
         + f"{str(greedy_av) + '%' : <22}"

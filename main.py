210babab16a1fe4efc5fb0a20e633779cb07bf1e
Spamakin <anakin@icalyx.com>
2022-02-08 14:26:05 -0600

    rough implementation of transfers and swaps

--- main.py
+++ main.py
@@ -14,21 +14,27 @@ graph_dict = TypedDict(
 
 
 def main() -> None:
-    n: int = 50
-    k: int = 5
+    n: int = 12
+    k: int = 4
     g = Graph.random_complete_metric(n, directed=False)
+    
+    # TODO: Create function to generate partition
+    # TODO: Create function to evalute before and after of transfers/swaps
+
     partition: list[set[int]] = [set() for _ in range(k)]
     for i in range(n):
         partition[random.randint(0, 1000) % k].add(i)
 
-    before: str = f"Before: {algos.max_average_cycle_length(g, partition)}\n"
+    # before: str = f"Before: {algos.nearest_neighbor(g, partition)}\n"
+    before: str = ""
     for i in range(len(partition)):
         before += f"    Agent {i}: {partition[i]}\n"
     print(before)
 
-    res = algos.improve_partition(g, partition)
+    res = algos.transfers_and_swaps_mwlp(g, partition, algos.nearest_neighbor)
 
-    after: str = f"After: {algos.max_average_cycle_length(g, res)}\n"
+    # after: str = f"After: {algos.nearest_neighbor(g, res)}\n"
+    after: str = ""
     for i in range(len(res)):
         after += f"    Agent {i}: {res[i]}\n"
     print(after)

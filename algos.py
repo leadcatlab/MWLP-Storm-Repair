1b7fe57198fa7f3872688d19876f0b47b51bead7
Spamakin <anakin@icalyx.com>
2022-03-31 10:08:35 -0500

    Removed dead benchmark code

--- algos.py
+++ algos.py
@@ -8,20 +8,6 @@ from more_itertools import set_partitions
 
 from graph import Graph
 
-
-class Bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-    CLEAR_LAST_LINE = "\033[A                                        \033[A"
-
-
 def path_length(g: Graph, path: list[int]) -> float:
     length: float = 0.0
     for i in range(len(path) - 1):

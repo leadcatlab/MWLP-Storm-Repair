eede83fd9df95174a2e49bc30d38bce92b948028
Spamakin <anakin@icalyx.com>
2022-05-30 09:23:29 -0500

    trying to make larger legend

--- benchmark.py
+++ benchmark.py
@@ -693,7 +693,7 @@ def line_plot(
         lines.append(line)
 
     mplcursors.cursor(lines, highlight=True)
-    plt.legend()
+    plt.legend(prop={'size': 6})
     figure = plt.gcf()
     figure.set_size_inches(10, 7)  # horizontal x vertical
     plt.title("Number of Unvisited People over Time")
